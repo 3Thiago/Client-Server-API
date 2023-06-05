d415b3ee425eee02a67dd0681f25e3030ad40b8d
Thiago Silva <correadasilvathiago@gmail.com>
2023-06-04 23:02:59 -0300

    Cliente.go created

--- Client/main.go
+++ Client/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main(){
-	
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	CreateFile(string(body))
+
+}
+
+func CreateFile(cotacao string) {
+	filename := "cotacao.txt"
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+	}
+
+	err := ioutil.WriteFile(filename, []byte("Dolar:"+cotacao), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
